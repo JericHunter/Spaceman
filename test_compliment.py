3139f30370a236028e589a1b2908132034d3daa5
JericHunter <jeric.hunterr@gmail.com>
2019-09-18 23:56:52 -0700

    pytest v2

--- test_compliment.py
+++ test_compliment.py
@@ -1,22 +1,23 @@
-import unittest
+# import unittest
 from spaceman import is_word_guessed
 from spaceman import get_guessed_word
 from spaceman import is_guess_in_word
 
 
-class getWord(unittest.TestCase):
-    def t_is_word_guessed(self):
-        assert is_word_guessed('apple', 'apple') is True
-        assert is_word_guessed('muy', 'bien') is False
-
-    def t_get_guessed_word(self):
-        assert get_guessed_word('apple', 'apple') == 'apple'
-        assert get_guessed_word('orange', 'apple') == 'a___e'
-
-    def t_is_guess_in_word(self):
-        assert is_guess_in_word('a', 'orange') is True
-        assert is_guess_in_word('o', 'apple') is False
+def test_is_word_guessed():
+    assert is_word_guessed('apple', 'apple') == True
+    assert is_word_guessed('muy', 'bien') == False
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_get_guessed_word():
+    assert get_guessed_word('apple', 'apple') == 'apple'
+    assert get_guessed_word('orange', 'apple') == 'a___e'
+
+
+def test_is_guess_in_word():
+    assert is_guess_in_word('a', 'orange') == True
+    assert is_guess_in_word('o', 'apple') == False
+#
+# if __name__ == '__main__':
+#     secret_word = load_word()
+#     spaceman(secret_word)
